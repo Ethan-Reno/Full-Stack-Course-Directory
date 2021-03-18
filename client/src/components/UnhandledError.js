4b185612ac3727f394df0b8f5b6de5e7a9d34548
Ethan Reno <epreno@live.ca>
2021-03-18 17:21:41 -0400

    Add UnhandledError component

--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-//UPDATE
 const UnhandledError = () => {
     return(
         <div className="bounds">
             <h1>Error</h1>
-            <p>Sorry! We just encountered an unexpected error.</p>
+            <p>There has been an error</p>
+            <Link className="button" to="/">Back to course directory</Link>
         </div>
     )
 }
